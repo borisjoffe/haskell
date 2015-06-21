6f6ea0d33acd2460d4e1e49061a15367dba67eec
Boris Joffe <boris@joff3.com>
2015-06-20 21:34:14 -0700

    ch3 part 1 exercises

--- scrap.hs
+++ scrap.hs
@@ -1,7 +1,26 @@
 #!/usr/bin/runhaskell
 
-secondToLast :: [a] -> a
-secondToLast xs = head (drop 1 (reverse (xs)))
+-- Chapter 3 Part 1
+-------------------
+-- file: ch03/ListADT.hs
+data List a = Cons a (List a)
+            | Nil
+              deriving (Show)
+
+-- exercise 1
+toList (Cons x (xs)) = x : toList xs
+toList Nil = []
+
+-- exercise 2
+data Tree a = Node {
+		  treeVal   :: a
+		, treeLeft  :: (Maybe (Tree a))
+		, treeRight :: (Maybe (Tree a))
+	} deriving (Eq, Show)
+
+
+-- Chapter 2 Part 2
+-------------------
 
 -- type synonyms
 -- NOT data constructors, just more descriptive types
@@ -11,8 +30,11 @@ type Author = String
 type CustomerId = Int
 type ReviewBody = String
 
-data Book = Book Id Title [Author]
-	deriving (Eq, Show)
+data Book = Book {
+		  bookId      :: Id
+		, bookTitle   :: Title
+		, bookAuthors :: [Author]
+	} deriving (Eq, Show)
 
 myBook = Book 123 "Title1" ["Author1"]
 
@@ -20,3 +42,10 @@ myBook = Book 123 "Title1" ["Author1"]
 -- since they're stored in separate namespaces
 data BookReview = BookReview Book CustomerId ReviewBody
 	deriving (Show)
+
+
+-- Chapter 2 Part 1
+-------------------
+
+secondToLast :: [a] -> a
+secondToLast xs = head (drop 1 (reverse (xs)))
