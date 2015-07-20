19da3f3bc8a8404043002a288fa62b28699d0cad
Boris Joffe <boris@joff3.com>
2015-07-20 11:48:17 -0700

    tabs to spaces

--- scrap.hs
+++ scrap.hs
@@ -9,9 +9,9 @@ import Data.Ord
 
 -- Ex 9
 data Point = Point {
-		  xCoord :: Double
-		, yCoord :: Double
-	} deriving (Show, Eq)
+          xCoord :: Double
+        , yCoord :: Double
+    } deriving (Show, Eq)
 
 data PointTriple = PointTriple {
           firstPoint  :: Point
@@ -20,24 +20,24 @@ data PointTriple = PointTriple {
     } deriving (Show)
 
 data Direction = CCW | CW | Collinear
-	deriving (Show, Eq)
+    deriving (Show, Eq)
 
 -- Ex 10
 
 getDirection :: Point -> Point -> Point -> Direction
 getDirection (Point x_a y_a) (Point x_b y_b) (Point x_c y_c)
-	| z > 0  = CCW
-	| z < 0  = CW
-	| z == 0 = Collinear
-	where
-		-- vector AB
-		x_ab = x_b - x_a
-		y_ab = y_b - y_a
-		-- vector BC
-		x_bc = x_c - x_b
-		y_bc = y_c - y_b
-		-- cross product
-		z = (x_ab * y_bc) - (y_ab * x_bc)
+    | z > 0  = CCW
+    | z < 0  = CW
+    | z == 0 = Collinear
+    where
+        -- vector AB
+        x_ab = x_b - x_a
+        y_ab = y_b - y_a
+        -- vector BC
+        x_bc = x_c - x_b
+        y_bc = y_c - y_b
+        -- cross product
+        z = (x_ab * y_bc) - (y_ab * x_bc)
 
 getDirection' :: PointTriple -> Direction
 getDirection' (PointTriple p1 p2 p3) =
@@ -49,19 +49,19 @@ nth num xs = head (drop num xs)
 
 getAllDirections :: [Point] -> [Direction]
 getAllDirections ps =
-	if length ps < 3
-	then []
-	else getDirection (nth 0 ps) (nth 1 ps) (nth 2 ps) : getAllDirections (tail ps)
+    if length ps < 3
+    then []
+    else getDirection (nth 0 ps) (nth 1 ps) (nth 2 ps) : getAllDirections (tail ps)
 
 -- Ex 12
 
 lowestYThenLowestX :: Point -> Point -> Ordering
 lowestYThenLowestX a b =
-	if compareY == EQ
-	then compare (xCoord a) (xCoord b)
-	else compareY
-	where
-		compareY = compare (yCoord a) (yCoord b)
+    if compareY == EQ
+    then compare (xCoord a) (xCoord b)
+    else compareY
+    where
+        compareY = compare (yCoord a) (yCoord b)
 
 -- TODO: implement
 uniq :: [Point] -> [Point]
@@ -85,7 +85,7 @@ getRelativePolarAngleWithXAxis p = yCoord p / xCoord p
 -- Get point with minimum y coordinate; if multiple points have the minimum, use the one with smallest x
 getLowestY :: [Point] -> Point
 getLowestY ps =
-	minimumBy (lowestYThenLowestX) ps
+    minimumBy (lowestYThenLowestX) ps
 
 isCcw :: PointTriple -> Bool
 isCcw pt = getDirection' pt == CCW
@@ -99,24 +99,24 @@ a, b, c, d
 makeTriples :: [a] -> [[a]]
 makeTriples xs
     | length xs < 3 = [[]] -- or [xs]???
-	| otherwise = [(nth 0 xs), (nth 1 xs), (nth 2 xs)] : makeTriples (tail xs)
+    | otherwise = [(nth 0 xs), (nth 1 xs), (nth 2 xs)] : makeTriples (tail xs)
 
 pointTriplesFromPoints :: [Point] -> [PointTriple]
 pointTriplesFromPoints ps
     | length ps < 3 = []
-	| otherwise = (PointTriple (nth 0 ps) (nth 1 ps) (nth 2 ps)) : pointTriplesFromPoints (tail ps)
+    | otherwise = (PointTriple (nth 0 ps) (nth 1 ps) (nth 2 ps)) : pointTriplesFromPoints (tail ps)
 
 pointsFromPointTriples :: [PointTriple] -> [Point]
 pointsFromPointTriples [] = []
-pointsFromPointTriples (pt : pts) = [firstPoint pt] ++ [secondPoint pt] ++ [thirdPoint pt] ++ pointsFromPointTriples pts
+pointsFromPointTriples (pt : pts) = firstPoint pt : secondPoint pt : thirdPoint pt : pointsFromPointTriples pts
 
 getHullFromPoints :: [Point] -> [Point]
 getHullFromPoints ps =
     -- when 3 points are CCW, add points to hull, if collinear, discard middle point
     -- filter points that are not ccw out
-    uniq (pointsFromPointTriples pointTriples)
+    uniq (pointsFromPointTriples ccwPointTriples)
     where
-        pointTriples = filter (isCcw) (pointTriplesFromPoints ps)
+        ccwPointTriples = filter (isCcw) (pointTriplesFromPoints ps)
 
 grahamScan :: [Point] -> [Point]
 grahamScan ps =
@@ -128,29 +128,31 @@ grahamScan ps =
         isLowestPoint p = (p == lowestPoint)
 
 
+-- TODO: add test
+
 -- Ex 8
 data MyTree a = MyNode a (MyTree a) (MyTree a)
             | Empty
-			  deriving (Show)
+              deriving (Show)
 
 treeHeight :: MyTree t -> Int
 treeHeight Empty = 0
 treeHeight (MyNode n leftTree rightTree) =
-	1 + max (treeHeight leftTree) (treeHeight rightTree)
+    1 + max (treeHeight leftTree) (treeHeight rightTree)
 
 testTreeHeight =
-	do
-		assert (treeHeight Empty)                     0
-		assert (treeHeight leaf)                      1
-		assert (treeHeight onlyLeft)                  2
-		assert (treeHeight onlyRight)                 2
-		assert (treeHeight both)                      2
-		assert (treeHeight (MyNode 5 onlyLeft Empty)) 3
-		where
-			leaf      = MyNode 5 Empty Empty
-			onlyLeft  = MyNode 5 leaf Empty
-			onlyRight = MyNode 5 Empty leaf
-			both      = MyNode 5 leaf leaf
+    do
+        assert (treeHeight Empty)                     0
+        assert (treeHeight leaf)                      1
+        assert (treeHeight onlyLeft)                  2
+        assert (treeHeight onlyRight)                 2
+        assert (treeHeight both)                      2
+        assert (treeHeight (MyNode 5 onlyLeft Empty)) 3
+        where
+            leaf      = MyNode 5 Empty Empty
+            onlyLeft  = MyNode 5 leaf Empty
+            onlyRight = MyNode 5 Empty leaf
+            both      = MyNode 5 leaf leaf
 
 -- Ex 7
 myIntersperse :: [a] -> [[a]] -> [a]
@@ -159,23 +161,23 @@ myIntersperse separator [x]      = x
 myIntersperse separator (x : xs) = x ++ separator ++ (myIntersperse separator xs)
 
 testMyIntersperse =
-	do
-		assert (myIntersperse "," []) []
-		assert (myIntersperse "," ["a"]) "a"
-		assert (myIntersperse "," ["abc","def"]) "abc,def"
+    do
+        assert (myIntersperse "," []) []
+        assert (myIntersperse "," ["a"]) "a"
+        assert (myIntersperse "," ["abc","def"]) "abc,def"
 
 -- mini test framework
 assert :: (Eq a, Show a) => a -> a -> IO ()
 assert actual expected =
-	if actual /= expected
-	then putStrLn ("\nERROR: Expected: " ++ show expected ++ " but got: " ++ show actual)
-	else putStr "."
+    if actual /= expected
+    then putStrLn ("\nERROR: Expected: " ++ show expected ++ " but got: " ++ show actual)
+    else putStr "."
 
 test :: IO a -> IO ()
 test func =
-	do
-		func
-		putStr "\n"
+    do
+        func
+        putStr "\n"
 -- end test framework
 
 
@@ -189,17 +191,17 @@ compareListLength xs ys = compare (length xs) (length ys)
 
 -- Ex5
 isPalindrome list =
-	case list of
-	[]       -> True
-	[x]      -> True
-	(x : xs) ->
-		x == last xs
-		&&
-		isPalindrome allButFirstAndLast
-		where
-			allButFirstAndLast = drop (len - 1) allButLast
-			allButLast         = take (len - 1) xs
-			len = length xs
+    case list of
+    []       -> True
+    [x]      -> True
+    (x : xs) ->
+        x == last xs
+        &&
+        isPalindrome allButFirstAndLast
+        where
+            allButFirstAndLast = drop (len - 1) allButLast
+            allButLast         = take (len - 1) xs
+            len = length xs
 
 -- Ex4
 palindrome xs = xs ++ reverse xs
@@ -227,10 +229,10 @@ toList Nil = []
 
 -- exercise 2
 data Tree a = Node {
-		  treeVal   :: a
-		, treeLeft  :: (Maybe (Tree a))
-		, treeRight :: (Maybe (Tree a))
-	} deriving (Eq, Show)
+          treeVal   :: a
+        , treeLeft  :: (Maybe (Tree a))
+        , treeRight :: (Maybe (Tree a))
+    } deriving (Eq, Show)
 
 
 -- Chapter 2 Part 2
@@ -245,17 +247,17 @@ type CustomerId = Int
 type ReviewBody = String
 
 data Book = Book {
-		  bookId      :: Id
-		, bookTitle   :: Title
-		, bookAuthors :: [Author]
-	} deriving (Eq, Show)
+          bookId      :: Id
+        , bookTitle   :: Title
+        , bookAuthors :: [Author]
+    } deriving (Eq, Show)
 
 myBook = Book 123 "Title1" ["Author1"]
 
 -- normal to have type/data constructor share the same name
 -- since they're stored in separate namespaces
 data BookReview = BookReview Book CustomerId ReviewBody
-	deriving (Show)
+    deriving (Show)
 
 
 -- Chapter 2 Part 1
@@ -263,6 +265,6 @@ data BookReview = BookReview Book CustomerId ReviewBody
 
 secondToLast :: [a] -> a
 secondToLast xs =
-	if length xs < 2
-	then error ("xs must have length of 2 or more but had length: " ++ show (length xs))
-	else head (drop 1 (reverse (xs)))
+    if length xs < 2
+    then error ("xs must have length of 2 or more but had length: " ++ show (length xs))
+    else head (drop 1 (reverse (xs)))
